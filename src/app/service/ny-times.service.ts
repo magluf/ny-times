e7fe5139eaa8142371cf50b332dd3a90875e913d
magluf <smagluf@gmail.com>
2019-09-22 00:08:11 -0300

    Removed stupid credential thing + docker stuff.

--- src/app/service/ny-times.service.ts
+++ src/app/service/ny-times.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { apiKey } from './credentials'; // gitignored.
 
 @Injectable()
 export class NyTimesService {
   private url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+  private apiKey = '3LHAOnUFTRGnsksTOfJGpUGicAnbetwW';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   async getHomeTopStories() {
-    return await this.http.get(`${this.url}?api-key=${apiKey}`).toPromise();
+    return await this.http.get(`${this.url}?api-key=${this.apiKey}`).toPromise();
   }
 }
